fb2807f7a7f7a79928ecbba1a9bdef35585ec7ea
abijij <105397465+abijij@users.noreply.github.com>
2024-01-14 23:53:47 -0600

    Final

--- src/main/java/EjemploOrdenes.java
+++ src/main/java/EjemploOrdenes.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class EjemploOrdenes {
+import java.util.Date;
+
+public class EjemploOrdenes {
+    public static void main(String[] args) {
+
+       OrdenCompra orden1 = new OrdenCompra("Se realizo la compra de componentes de una pc");
+       orden1.setCliente(new Cliente("Edgar", "Gutierrez"));
+       orden1.setFecha(new Date());
+
+       orden1.addProducto(new Producto("Kingston","Disco duro SSD M2", 680));
+        orden1.addProducto(new Producto("Machenike","Teclado Mecanico", 2700));
+        orden1.addProducto(new Producto("Razer","Mouse Jummper", 780));
+        orden1.addProducto(new Producto("Razer","Razer Nagga", 800));
+
+        OrdenCompra orden2 = new OrdenCompra("Se realizo mantenimiento de una pc ");
+        orden2.setCliente(new Cliente("Gabriela","Ponce Hernanadez"));
+        orden2.setFecha(new Date());
+
+         orden2.addProducto(new Producto("Posdevv","Cambio de pasta termica",300));
+         orden2.addProducto(new Producto("Posdevv","Limpieza profundfa", 700));
+         orden2.addProducto(new Producto("Posdevv", "Cambio de fuente", 1500));
+         orden2.addProducto(new Producto("Posdevv","Cambio de procesador", 5700));
+
+         OrdenCompra orden3 = new OrdenCompra("Creacion de Pagina web");
+         orden3.setCliente(new Cliente("Antuan","Gutierrez Ponce"));
+         orden3.setFecha(new Date());
+
+         orden3.addProducto(new Producto("Possdevv", "Definicion de poryecto",18000));
+         orden3.addProducto(new Producto("Possdevv","Definicion de tegnologias", 10000));
+         orden3.addProducto(new Producto("Posdevv","Se empieza desarrollo", 30000));
+         orden3.addProducto(new Producto("Possdevv","Primera entrega de avances",24000));
+
+            OrdenCompra[] ordenes = {orden1,orden2,orden3};
+
+        for (OrdenCompra orden : ordenes) {
+            System.out.println("Folio: "+ orden.getId());
+            System.out.println("Cliente: " + orden.getCliente());
+            System.out.println("Descripcion" + orden.getDescripcion());
+            System.out.println("Fecha: " + orden.getFecha());
+            System.out.println("Total: " + orden.totalCompra());
+            int i = 1;
+            for (Producto p : orden.getProductos()) {
+                System.out.println("Producto " + i + ": " + p.getNombre() + " " + p.getFabricante() + " precio $" + p.getPrecio());
+                i++;
+            }
+        }
+
+        System.out.println("---------------------------------- siguiente --------------------------------------");
+
+
+    }
 }
+
+
+
--- src/main/java/OrdenCompra.java
+++ src/main/java/OrdenCompra.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class OrdenCompra {
+import java.util.Date;
+
+public class OrdenCompra {
+    private int id;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private Producto[] productos;
+    private static int ultimoId;
+    private int indiceProducto;
+
+
+
+    public OrdenCompra(String descripcion) {
+        this.descripcion = descripcion;
+        this.id = ++ultimoId;
+        this.productos = new Producto[4];
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Producto[] getProductos() {
+        return productos;
+    }
+
+    public static int getUltimoId() {
+        return ultimoId;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void addProducto(Producto producto){
+        if (indiceProducto < this.productos.length){
+            this.productos[indiceProducto++] = producto;
+        }
+    }
+
+    public int totalCompra(){
+            int total = 0;
+        for (Producto p: this.productos) {
+            if (p != null) {
+                total += p.getPrecio();
+            }
+        }
+        return total;
+    }
 }
